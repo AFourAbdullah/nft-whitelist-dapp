196b8680ea3656f3755bc0bf636b73f7db6c0777
Abdullah Aziz <abdullahazees4@gmail.com>
2023-04-14 22:49:50 +0500

    project completed

--- my-first-dapp/src/pages/index.js
+++ my-first-dapp/src/pages/index.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Contract, providers } from "ethers";
 import { WHITELIST_CONTRACT_ADDRESS, abi } from "../../constants";
 import Head from "next/head";
 import styles from "../styles/home.module.css";
+import Web3Modal from "web3modal";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +15,7 @@ export default function Home() {
   const [loading, setloading] = useState(false);
   const [numOfWhitelised, setnumOfWhitelised] = useState(0);
   const web3ModalRef = useRef();
-  const getProviderOrSigner = async (getSigner = false) => {
+  const getProviderOrSigner = async (needSigner = false) => {
     const provider = await web3ModalRef.current.connect();
     const web3provider = new providers.Web3Provider(provider);
     const { chainId } = await web3provider.getNetwork();
@@ -50,7 +51,7 @@ export default function Home() {
       const whitelistContract = new Contract(
         WHITELIST_CONTRACT_ADDRESS,
         abi,
-        signer
+        provider
       );
       const _numOfWhiteListed =
         await whitelistContract.numOfAddressesWhitelisted();
@@ -84,6 +85,38 @@ export default function Home() {
       console.log(error);
     }
   };
+  const renderButton = () => {
+    if (WalletConnected) {
+      if (joinedWhiteList) {
+        return;
+        <div className={styles.description}>
+          Thanks for joining the Whitelist!
+        </div>;
+      } else if (loading) {
+        return <button className={styles.button}>Loading...</button>;
+      } else {
+        return (
+          <button onClick={addToWhitelist} className={styles.button}>
+            Join the Whitelist
+          </button>
+        );
+      }
+    } else {
+      <button onClick={connectWallet} className={styles.button}>
+        Connect Wallet
+      </button>;
+    }
+  };
+  useEffect(() => {
+    if (!WalletConnected) {
+      web3ModalRef.current = new Web3Modal({
+        network: "goerli",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+      connectWallet();
+    }
+  }, [WalletConnected]);
   return (
     <div>
       <Head>
@@ -91,6 +124,25 @@ export default function Home() {
         <meta name="description" content="Whitelist-Dapp" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className={styles.main}>
+        <div>
+          <h1 className={styles.title}>Welcome to Crypto Devs!</h1>
+          <div className={styles.description}>
+            {/* Using HTML Entities for the apostrophe */}
+            It&#39;s an NFT collection for developers in Crypto.
+          </div>
+          <div className={styles.description}>
+            {numOfWhitelised} has already joined the whitelist
+          </div>
+          {renderButton()}
+        </div>
+        <div>
+          <img className={styles.image} src="./cryptodevs.svg" />
+        </div>
+      </div>
+      <footer className={styles.footer}>
+        Made with &#10084; by Crypto Devs
+      </footer>
     </div>
   );
 }
